14652938102507b688e0e8db17fc8320ae3a8a2c
Alessandro <a.sgherri92@gmail.com>
2022-08-23 18:36:09 +0200

    JSX-05

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+function sum (a, b){
+    const result = <h2>{a + b}</h2>
+    return result;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(sum(19, 31));
