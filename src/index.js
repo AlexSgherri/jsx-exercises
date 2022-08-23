48b9175a1182e194f1d412732b060cf491ef4689
Alessandro <a.sgherri92@gmail.com>
2022-08-23 18:30:12 +0200

    jsx-02

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+function ChoseName (name){
+    const nameTag = <h1>Hello, {name}</h1>
+    return nameTag;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(ChoseName("Vittorio"));
